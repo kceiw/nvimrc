6bcf045fc2d203408721a3b46623e11b0956eb0f
kceiw <hellomao@outlook.com>
2023-03-15 22:05:36 -0700

    clean style

--- lua/plugins/find.lua
+++ lua/plugins/find.lua
@@ -10,7 +10,7 @@ return {
       local keymap_opts = {
         noremap = true
       }
-      vim.keymap.set("n", "<Leader>ff", builtin.find_files, keymap_opts) 
+      vim.keymap.set("n", "<Leader>ff", builtin.find_files, keymap_opts)
       vim.keymap.set("n", "<Leader>fb", builtin.buffers, keymap_opts)
       vim.keymap.set("n", "<Leader>fg", builtin.live_grep, keymap_opts)
       vim.keymap.set("n", "<Leader>fh", builtin.help_tags, keymap_opts)
