466328da56c296abf036ca54896b3f1acb788264
kceiw <hellomao@outlook.com>
2023-03-17 21:45:59 -0700

    Add a few highlight plugins.

--- lua/plugins/highlight.lua
+++ lua/plugins/highlight.lua
@@ -40,5 +40,27 @@ return {
         enable = true
       },
     }
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = true,
+  },
+  {
+    "HiPhish/nvim-ts-rainbow2",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        rainbow = {
+          enable = true,
+          -- list of languages you want to disable the plugin for
+          disable = { },
+          -- Which query to use for finding delimiters
+          query = 'rainbow-parens',
+          -- Highlight the entire buffer all at once
+          strategy = require 'ts-rainbow.strategy.global',
+        }
+      })
+    end,
   }
 }
--- lua/plugins/pair.lua
+++ lua/plugins/pair.lua
@@ -3,4 +3,16 @@ return {
     "windwp/nvim-autopairs",
     config = true,
   },
+  {
+    "andymass/vim-matchup",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        matchup = {
+          enable = true,              -- mandatory, false will disable the whole extension
+          disable = { },  -- optional, list of language that will be disabled
+          -- [options]
+        },
+      })
+    end,
+  }
 }
