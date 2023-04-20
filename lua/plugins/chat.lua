ba8a0043fd90612bddce52361e40a2c4795ec558
kceiw <hellomao@outlok.com>
2023-04-20 11:40:45 -0700

    add mastodon plugin

---
+++ lua/plugins/chat.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "kode-team/mastodon.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "rcarriga/nvim-notify",
+      "kkharji/sqlite.lua",
+    },
+    config = true,
+  }
+}
+
