0c9317c2c815abdd306e5041f2f4ebdd05225d05
kceiw <hellomao@outlook.com>
2023-05-02 19:06:52 +0000

    Update the way to update treesitter

--- lua/plugins/highlight.lua
+++ lua/plugins/highlight.lua
@@ -1,7 +1,9 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
+    build = function()
+      require("nvim-treesitter.install").update({ with_sync = true })
+    end,
     opts = {
       auto_install = true,
       ensure_installed = {
