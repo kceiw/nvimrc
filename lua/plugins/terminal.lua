f12b49847151e29c669f84138dcccacd6fad46d8
kceiw <hellomao@outlook.com>
2023-10-19 12:16:42 -0700

    Configure ToggleTerminal and key map
    - The terminal will be open in a tab by default.
    - Change terminal open mapping to C-s
    - Add window mapping to move amongst windows/terminals.

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -3,7 +3,8 @@ return {
     "akinsho/toggleterm.nvim",
     opts = {
       insert_mappings = true,
-      open_mapping = "<C-k>",
+      open_mapping = "<C-s>",
+      direction = "tab",
       terminal_mappings = true,
     }
   }
