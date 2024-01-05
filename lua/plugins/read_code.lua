d88d7ec9a439b414570183cf8cc632e8dc29e682
kceiw <hellomao@outlook.com>
2024-01-05 09:49:26 -0800

    Add the plugin to read GitHub pull requests.

--- lua/plugins/read_code.lua
+++ lua/plugins/read_code.lua
@@ -2,4 +2,19 @@ return {
   {
     "preservim/tagbar",
   },
+  {
+    "ldelossa/litee.nvim",
+    config = function()
+        require("litee.lib").setup()
+    end,
+  },
+  {
+    "ldelossa/gh.nvim",
+    dependencies = {
+      { "ldelossa/litee.nvim", }
+    },
+    config = function()
+        require("litee.gh").setup()
+    end,
+  },
 }
