1257f049338008365f4cb0173132219434817e27
kceiw <hellomao@outlook.com>
2024-11-27 14:16:42 -0800

    Use a new branch for venv-select

--- lua/plugins/file.lua
+++ lua/plugins/file.lua
@@ -120,7 +120,14 @@ return {
   {
     "coffebar/transfer.nvim",
     lazy = true,
-    cmd = { "TransferInit", "DiffRemote", "TransferUpload", "TransferDownload", "TransferDirDiff", "TransferRepeat" },
+    cmd = {
+      "TransferInit",
+      "DiffRemote",
+      "TransferUpload",
+      "TransferDownload",
+      "TransferDirDiff",
+      "TransferRepeat"
+    },
     opts = {},
   },
 }
--- lua/plugins/language.lua
+++ lua/plugins/language.lua
@@ -12,6 +12,7 @@ return {
   },
   {
     "linux-cultist/venv-selector.nvim",
+    branch = "regexp",
     dependencies = {
       "neovim/nvim-lspconfig",
       "nvim-telescope/telescope.nvim"
