0147cdfffb0eae949750e22677aaa6b7454de8c5
kceiw <hellomao@outlook.com>
2024-11-27 14:14:44 -0800

    Update configure for obsidian

--- lua/plugins/note.lua
+++ lua/plugins/note.lua
@@ -27,26 +27,31 @@ return {
         -- Trigger completion at 2 chars.
         min_chars = 2,
 
-        -- Where to put new notes created from completion. Valid options are
-        --  * "current_dir" - put new notes in same directory as the current buffer.
-        --  * "notes_subdir" - put new notes in the default notes subdirectory.
-        new_notes_location = "current_dir",
-
       },
 
-      -- Optional, customize how names/IDs for new notes are created.
+      -- Where to put new notes created from completion. Valid options are
+      --  * "current_dir" - put new notes in same directory as the current buffer.
+      --  * "notes_subdir" - put new notes in the default notes subdirectory.
+      new_notes_location = "current_dir",
+
+      -- Optional, customize how note IDs are generated given an optional title.
+      ---@param title string|?
+      ---@return string
       note_id_func = function(title)
-        local file_name= ""
+        -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+        -- In this case a note with the title 'My new note' will be given an ID that looks
+        -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+        local suffix = ""
         if title ~= nil then
           -- If title is given, transform it into valid file name.
-          file_name = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+          suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
         else
-          -- If title is nil, just add 4 random uppercase letters to the file name.
+          -- If title is nil, just add 4 random uppercase letters to the suffix.
           for _ = 1, 4 do
-            file_name = file_name .. string.char(math.random(65, 90))
+            suffix = suffix .. string.char(math.random(65, 90))
           end
         end
-        return file_name
+        return tostring(os.time()) .. "-" .. suffix
       end,
 
       -- Optional, set to true if you don't want Obsidian to manage frontmatter.
@@ -74,13 +79,55 @@ return {
       -- Optional, set to true to force ':ObsidianOpen' to bring the app to the foreground.
       open_app_foreground = true,
 
-      -- Optional, customize the backlinks interface.
-      backlinks = {
-        -- The default height of the backlinks pane.
-        height = 10,
-        -- Whether or not to wrap lines.
-        wrap = true,
+      -- Optional, customize how wiki links are formatted. You can set this to one of:
+      --  * "use_alias_only", e.g. '[[Foo Bar]]'
+      --  * "prepend_note_id", e.g. '[[foo-bar|Foo Bar]]'
+      --  * "prepend_note_path", e.g. '[[foo-bar.md|Foo Bar]]'
+      --  * "use_path_only", e.g. '[[foo-bar.md]]'
+      -- Or you can set it to a function that takes a table of options and returns a string, like this:
+      wiki_link_func = function(opts)
+        return require("obsidian.util").wiki_link_alias_only(opts)
+      end,
+
+      -- Optional, customize how markdown links are formatted.
+      markdown_link_func = function(opts)
+        -- return require("obsidian.util").markdown_link(opts)
+        -- The default one encode the path which doesn't show Chinese.
+        local anchor = ""
+        local header = ""
+        if opts.anchor then
+          anchor = opts.anchor.anchor
+          header = require("obsidian.util").format_anchor_label(opts.anchor)
+        elseif opts.block then
+          anchor = "#" .. opts.block.id
+          header = "#" .. opts.block.id
+        end
+
+        local path = opts.path
+        return string.format("[%s%s](%s%s)", opts.label, header, path, anchor)
+      end,
+
+      -- Either 'wiki' or 'markdown'.
+      preferred_link_style = "markdown",
+
+      picker = {
+        -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+        name = "telescope.nvim",
+        -- Optional, configure key mappings for the picker. These are the defaults.
+        -- Not all pickers support all mappings.
+        mappings = {
+          -- Create a new note from your query.
+          new = "<C-x>",
+          -- Insert a link to the selected note.
+          insert_link = "<C-l>",
+        },
       },
+
+      -- Optional, sort search results by "path", "modified", "accessed", or "created".
+      -- The recommend value is "modified" and `true` for `sort_reversed`, which means, for example,
+      -- that `:ObsidianQuickSwitch` will show the notes sorted by latest modified time
+      sort_by = "modified",
+      sort_reversed = true,
     },
   }
 }
