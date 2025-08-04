5371280d282beddf11f20f6d073ad3796ca636e8
kceiw <hellomao@outlook.com>
2025-08-03 23:21:10 -0700

    Move hop.nvim from key.lua to find.lua

--- lua/plugins/find.lua
+++ lua/plugins/find.lua
@@ -18,4 +18,33 @@ return {
       vim.keymap.set("n", "<Leader>fh", builtin.help_tags, keymap_opts)
     end
   },
+  {
+    "smoka7/hop.nvim",
+
+    init = function()
+      local hop = require("hop")
+      local directions = require("hop.hint").HintDirection
+      vim.keymap.set("", "f", function()
+        hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+      end, {remap=true})
+      vim.keymap.set("", "F", function()
+        hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+      end, {remap=true})
+      vim.keymap.set("", "t", function()
+        hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+      end, {remap=true})
+      vim.keymap.set("", "T", function()
+        hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+      end, {remap=true})
+    end,
+    config = function()
+      -- you can configure Hop the way you like here; see :h hop-config
+      require("hop").setup({
+        keys = "etovxqpdygfblzhckisuran",
+        extensions = {
+          "hop-zh-by-flypy"
+        },
+      })
+    end
+  },
 }
