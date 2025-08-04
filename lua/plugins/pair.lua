3e2d3c685f5adf2fa4dc6c0f1be0da60621f1107
kceiw <hellomao@outlook.com>
2025-08-03 23:03:06 -0700

    Update matchup settings

--- lua/plugins/pair.lua
+++ lua/plugins/pair.lua
@@ -5,14 +5,17 @@ return {
   },
   {
     "andymass/vim-matchup",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        matchup = {
-          enable = true,              -- mandatory, false will disable the whole extension
-          disable = { "lua" },  -- optional, list of language that will be disabled
-          -- [options]
-        },
-      })
+    init = function()
+      vim.g.matchup_treesitter_stopline = 500
+      -- To enable the delete surrounding (ds%) and change surrounding (cs%) maps
+      vim.g.matchup_surround_enabled = 1
+      -- To enable the experimental transmute module: In insert mode, after changing text inside a word,
+      -- matching words will be changed in parallel
+      vim.g.matchup_transmute_enabled = 1
+      -- To disable matching withing strings and comments but recognize symbols within comments
+      -- Value 2 to indicate not to recognize anything in comment.
+      vim.g.matchup_delim_noskips = 1
     end,
+    config = true,
   }
 }
