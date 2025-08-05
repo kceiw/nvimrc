74405015d62dd1e79401584326de023648a4595b
kceiw <hellomao@outlook.com>
2025-08-04 23:57:19 -0700

    Revert "Add support for lsp in nvim_tree"
    This reverts commit f6702a62b793c529cfdc7df95fa7e037c375635b.

--- lua/plugins/file.lua
+++ lua/plugins/file.lua
@@ -56,16 +56,6 @@ return {
     end,
   },
   {
-    "antosha417/nvim-lsp-file-operations",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-tree.lua",
-    },
-    config = function()
-      require("lsp-file-operations").setup()
-    end,
-  },
-  {
     "coffebar/transfer.nvim",
     lazy = true,
     cmd = {
