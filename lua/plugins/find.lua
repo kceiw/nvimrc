9731c334a55235713142be0cc761ea9a106f9578
kceiw <hellomao@outlook.com>
2025-08-05 23:06:51 -0700

    Replace telescope with fzf-lua

--- lua/plugins/find.lua
+++ lua/plugins/find.lua
@@ -1,26 +1,20 @@
 return {
   {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim"
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      { "<Leader>ff", "<cmd>FzfLua files<cr>", "FZF files" },
+      { "<Leader>fb", "<cmd>FzfLua buffers<cr>", "FZF buffers" },
+      { "<Leader>fg", "<cmd>FzfLua live_grep<cr>", "FZF live grep" },
+      { "<Leader>fm", "<cmd>FzfLua git_commits<cr>", "FZF git commits" },
+      { "<Leader>fh", "<cmd>FzfLua helptags<cr>", "FZF helptags" },
+      { "<Leader>ft", "<cmd>FzfLua tags<cr>", "FZF tags" },
     },
-
-    config = true,
-
-    init = function()
-      local builtin = require("telescope.builtin")
-      local keymap_opts = {
-        noremap = true
-      }
-      vim.keymap.set("n", "<Leader>ff", builtin.find_files, keymap_opts)
-      vim.keymap.set("n", "<Leader>fb", builtin.buffers, keymap_opts)
-      vim.keymap.set("n", "<Leader>fg", builtin.live_grep, keymap_opts)
-      vim.keymap.set("n", "<Leader>fh", builtin.help_tags, keymap_opts)
-    end
+    opts = {}
   },
   {
     "smoka7/hop.nvim",
-
     init = function()
       local hop = require("hop")
       local directions = require("hop.hint").HintDirection
@@ -46,5 +40,7 @@ return {
         },
       })
     end
-  },
+  }
 }
+
+
